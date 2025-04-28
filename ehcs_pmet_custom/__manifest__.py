15ffad9733ddf2a9915971c420b8f16d584da44b
uday-erphabor <uday.b.erpharbor@gmail.com>
2025-04-28 14:25:06 +0530

    [ADD]Added new module.

---
+++ ehcs_pmet_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'EHCS PMET CUSTOM',
+    'author': 'ERP Harbor Consulting Services',
+    'category': 'Sales',
+    'summary': 'EHCS Tube Filling Log',
+    'website': 'http://www.erpharbor.com',
+    'version': '18.0.1.0.0',
+    'description': "",
+    'depends': ['hr'],
+    'demo' : [],
+    "data" : [   
+        # 'security/ir.model.access.csv',
+        # 'data/data.xml',
+        "views/hr.xml",
+    ],
+    'installable': True,
+}
